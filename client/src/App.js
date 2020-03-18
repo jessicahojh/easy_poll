e4e99e95aa99b0b74ab10eba538902e85f63e3dd
Jessica <jessica.ho.jh@gmail.com>
2020-03-17 21:02:34 -0700

    created questions, options, and vote models

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,15 @@ import { BrowserRouter as Router} from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import Poll from './components/Poll';
+import CreateForm from './components/CreateForm';
 
 const App = () => {
   return (
 
       <Router>
-
+  
           <Navbar/>
+          <CreateForm/>
           <Poll/>
 
       </Router>
