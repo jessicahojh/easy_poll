8e1930c8438c14accb8ff3cebbe2a9882713dfd1
Jessica <jessica.ho.jh@gmail.com>
2020-03-18 15:29:35 -0700

    redux actions setup

---
+++ client/src/actions/types.js
@@ -0,0 +1,14 @@
+export const REGISTER_USER = 'REGISTER_USER';
+export const LOGIN_USER = 'LOGIN_USER';
+export const LOGOUT_USER = 'LOGOUT_USER';
+
+export const GET_DB_QUESTIONS_AND_OPTIONS = 'GET_DB_QUESTIONS_AND_OPTIONS';
+
+export const ADD_QUESTION = 'ADD_QUESTION';
+export const DELETE_QUESTION = 'DELETE_QUESTION';
+export const SELECT_OPTION = 'SELECT_OPTION';
+export const EDIT_OPTION = 'EDIT_OPTION';
+export const DELETE_OPTION = 'DELETE_OPTION';
+
+
+
