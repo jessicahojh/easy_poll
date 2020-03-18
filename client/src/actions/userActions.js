182b0c4b71737c5da7cb8c8b7478daddf0be5598
Jessica <jessica.ho.jh@gmail.com>
2020-03-18 15:32:47 -0700

    created user actions and reducers

---
+++ client/src/actions/userActions.js
@@ -0,0 +1,36 @@
+import {
+    REGISTER_USER,
+    LOGIN_USER,
+    LOGOUT_USER
+  } from './types';
+  
+  // Register User to server
+  export const addUser = user => async dispatch => {
+    await fetch('/users/add', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    dispatch({
+      type: REGISTER_USER,
+      payload: user.username
+    });
+  };
+  
+  // Login the user if passed check
+  export const loginUser = (username) => dispatch => {
+    dispatch({
+      type: LOGIN_USER,
+      payload: username
+    });
+  };
+  
+  // Logout the user
+  export const logoutUser = () => dispatch => {
+    dispatch({
+      type: LOGOUT_USER
+    });
+  };
---
+++ client/src/reducers/userReducer.js
@@ -0,0 +1,36 @@
+import {
+    REGISTER_USER,
+    LOGIN_USER,
+    LOGOUT_USER
+  } from '../actions/types';
+  
+  const initialState = {
+    user: null,
+    isLogged: false
+  };
+  
+  export default (state = initialState, action) => {
+    switch(action.type) {
+  
+      case REGISTER_USER:
+        return {
+          user: action.payload,
+          isLogged: true
+        };
+  
+      case LOGIN_USER:
+        return {
+          user: action.payload,
+          isLogged: true
+        };
+  
+      case LOGOUT_USER:
+        return {
+          user: null,
+          isLogged: false
+        };
+  
+      default:
+        return state;
+    };
+  };
