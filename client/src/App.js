7759d94d8bae86b30a4a4894512d29d4c829aa6e
Jessica <jessica.ho.jh@gmail.com>
2020-03-19 16:43:12 -0700

    took code out to put in home component. also added routers and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,71 +1,37 @@
-import React, { useState } from 'react';
-import './App.css';
-import { BrowserRouter as Router} from 'react-router-dom';
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 
 import Navbar from './components/Navbar';
-import Poll from './components/Poll';
-import CreateForm from './components/CreateForm';
+import Home from './components/Home';
+import About from './components/About';
+import Login from './components/auth/Login';
+import Logout from './components/auth/Logout';
+import Register from './components/auth/Register';
 
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+
+import './App.css';
 
 const App = () => {
-
-  const [user, setUser] = useState("")
-  const [question, setQuestion] = useState("")
-  const [option1, setOption1] = useState("")
-  const [option2, setOption2] = useState("")
-  const [] = useState()
-  const [] = useState()
-
-  const handleQuestion = e => {
-    setQuestion(e.target.value)
-  }
-
-  const handleOption1 = e => {
-    setOption1(e.target.value)
-  }
-
-  const handleOption2 = e => {
-    setOption2(e.target.value)
-  }
-
-  const handleSubmit= e => {
-    e.preventDefault();
-    if (question !== "" && option1 !== "" && option2 !== "") {
-
-    }
-  }
-
   return (
     <Provider store={store}>
       <Router>
-
-        <Navbar/>
-
-        <Container>
-          <Row>
-            <Col></Col>
-            <Col xs={6}>
-              <CreateForm
-                handleQuestion={handleQuestion}
-                handleOption1={handleOption1}
-                handleOption2={handleOption2}
-                handleSubmit={handleSubmit}
-              />
-            </Col>
-            <Col></Col>
-          </Row>
-
-          <Poll/>
-
-        </Container>
+        <Fragment>
+          <Navbar/>
+          <Container>
+            <Switch>
+              <Route exact path='/' component={Home}/>
+              <Route exact path='/about' component={About}/>
+              <Route exact path='/login' component={Login}/>
+              <Route exact path='/logout' component={Logout}/>
+              <Route exact path='/register' component={Register}/>
+            </Switch>
+          </Container>
+        </Fragment>
       </Router>
     </Provider>
-
   );
 }
 
