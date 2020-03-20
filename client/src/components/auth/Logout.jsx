0e780d07a5fa8398aa01663fbd70c3675f6a8f9a
Jessica <jessica.ho.jh@gmail.com>
2020-03-19 17:40:32 -0700

    created working log out and register components

---
+++ client/src/components/auth/Logout.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { logoutUser } from '../../actions/userActions';
+
+const Logout = () => {
+  const isLogged = useSelector((state) => state.users.isLogged);
+
+  const dispatch = useDispatch();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(logoutUser());
+  };
+
+  return (
+    <div>
+      {
+        isLogged ?
+        <div className='form-container'>
+          <h1>Are you sure you want to log out?</h1>
+          <form onSubmit={onSubmit}>
+            <input
+              type='submit'
+              value='Logout'
+            />
+          </form>
+        </div>
+        : <h3>You are not logged in</h3>
+      }
+    </div>
+  );
+};
+
+export default Logout;
---
+++ client/src/components/auth/Register.jsx
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../../actions/userActions';
+import { useHistory } from 'react-router-dom';
+
+const Register = () => {
+  const [ username, setUsername ] = useState('');
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
+  let history = useHistory();
+
+  const dispatch = useDispatch();
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    const newUser = {
+      username,
+      email,
+      password,
+    }
+
+    dispatch(addUser(newUser))
+
+    // Clear Fields
+    setUsername('');
+    setEmail('');
+    setPassword('');
+
+    // reroute to Home
+    history.push('/')
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='name'>Username</label>&nbsp;
+          <input
+            id='username'
+            type='text'
+            name='username'
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>&nbsp;
+          <input
+            id='email'
+            type='email'
+            name='email'
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>&nbsp;
+          <input
+            id='password'
+            type='password'
+            name='password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+            minLength='6'
+            autoComplete="on"
+          />
+        </div>
+        <input
+          type='submit'
+          value='Register'
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Register;
