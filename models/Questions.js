459ddc2e03ea28b62cc4c365d9200e8d5b7835c0
Jessica <jessica.ho.jh@gmail.com>
2020-03-31 14:22:10 -0700

    added reference to User in Question schema

--- models/Questions.js
+++ models/Questions.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const QuestionSchema = mongoose.Schema({
-    userId: {type: String, required: true},
+    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
     question: {type: String, required: true},
     option1Id: {type: String, required: true},
     option2Id: {type: String, required: true}
 });
 
-module.exports = mongoose.model('user', QuestionSchema);
+module.exports = mongoose.model('question', QuestionSchema);
