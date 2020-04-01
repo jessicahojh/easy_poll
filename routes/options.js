ace92cc976149c426428f36d00b9a51eb77114a9
Jessica <jessica.ho.jh@gmail.com>
2020-04-01 16:49:17 -0700

    added routes to add options

---
+++ routes/options.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const router = express.Router();
+
+const Options = require('../models/Options');
+
+// @route   GET /optionss
+// @desc    Get all options
+// @access  Public
+
+router.get('/', (req, res) => {
+    res.send("Get all options");
+});
+
+// @route   POST /options
+// @desc    Add new option
+// @access  Public
+router.post('/add', async (req, res) => {
+
+    try {
+
+    const option = req.body.option;
+    const votes = []
+
+    const newOption = new Options({
+        option,
+        votes 
+      });
+
+    await newOption.save()
+    .then(res.json('Option added!'))
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router;
