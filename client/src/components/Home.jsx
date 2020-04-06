c3b064071029ec8a3a549741bcd8251cdfe721eb
Jessica <jessica.ho.jh@gmail.com>
2020-04-06 15:55:01 -0700

    deleted unwanted code

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -9,32 +9,6 @@ import Col from 'react-bootstrap/Col';
 
 const Home = () => {
 
-    const [user, setUser] = useState("")
-    const [question, setQuestion] = useState("")
-    const [option1, setOption1] = useState("")
-    const [option2, setOption2] = useState("")
-    const [] = useState()
-    const [] = useState()
-
-    const handleQuestion = e => {
-        setQuestion(e.target.value)
-    }
-
-    const handleOption1 = e => {
-        setOption1(e.target.value)
-    }
-
-    const handleOption2 = e => {
-        setOption2(e.target.value)
-    }
-
-    const handleSubmit= e => {
-        e.preventDefault();
-        if (question !== "" && option1 !== "" && option2 !== "") {
-
-        }
-    }
-
     return (
         <div>
 
@@ -42,12 +16,7 @@ const Home = () => {
             <Row>
                 <Col></Col>
                 <Col xs={6}>
-                <QuestionForm
-                    handleQuestion={handleQuestion}
-                    handleOption1={handleOption1}
-                    handleOption2={handleOption2}
-                    handleSubmit={handleSubmit}
-                />
+                <QuestionForm/>
                 </Col>
                 <Col></Col>
             </Row>
