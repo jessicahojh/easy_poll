5be5aefdbd26c584cdac52f113f68c9b5374888c
Jessica <jessica.ho.jh@gmail.com>
2020-05-27 14:52:10 -0700

    installed express-fileupload and created endpoints for file upload

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const fileUpload = require('express-fileupload');
 
 const app = express();
+
+app.use(fileUpload());
 
 // Connect Database
 connectDB();
@@ -16,6 +19,24 @@ app.use('/questions', require('./routes/questions'));
 app.use('/options', require('./routes/options'));
 app.use('/votes', require('./routes/votes'));
 
+
+// Upload Endpoint
+app.post('/upload', (req, res) => {
+    if (req.files === null) {
+      return res.status(400).json({ msg: 'No file uploaded' });
+    }
+  
+    const file = req.files.file;
+  
+    file.mv(`${__dirname}/client/public/uploads/${file.name}`, err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).send(err);
+      }
+  
+      res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+    });
+});
 
 const PORT = process.env.PORT || 5000;
 
