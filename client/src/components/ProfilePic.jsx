18678932e481e6f72eb27e3ace02fef3f63fab1b
Jessica <jessica.ho.jh@gmail.com>
2020-06-01 13:28:42 -0700

    created default pic for users without a profile pic

--- client/src/components/ProfilePic.jsx
+++ client/src/components/ProfilePic.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import pic from '../static/my_pic.jpg';
+import defaultPic from '../static/defaultPic.jpg';
 
 const ProfilePic = () => {
 
     const user = useSelector((state) => state.users.user);
 
-    if (user) {
+    if (user && user.photo !== "") {
         return (
             <img src={`/uploads/${user.photo}`} alt="profilepic" className='pic'/>
         )
     } else {
         return (
-            <div>No Pic</div>
+            <img src={defaultPic} alt="defaultpic" className='pic'/>
         )
     }
 }
